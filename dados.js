4a7d458011bda8bc1c9057884b0b47b532d3dee5
Matheus Oliveira <matheus.oliveira6842@gmail.com>
2024-01-25 23:39:26 -0300

    melhorias

--- dados.js
+++ dados.js
@@ -1,3 +1,4 @@
+
 function selected(value){
     var formRugasT = document.getElementsByClassName('form1')
     if(value =="Rugas - Transversais"){
@@ -5,27 +6,44 @@ function selected(value){
      }
 }
 function carregar(){
-    var fregiao = document.getElementsByName('regiao')
+   var selectElement = document.getElementById('severity')
+   var indiceOpition0 = 0;
+   var indiceOpition1 = 1; /*sev0*/
+   var indiceOpition2 = 2; /*sev1*/
+   var indiceOpition3 = 3; /*sev2*/
+   var indiceOpition4 = 4; /*sev3*/
+   var indiceOpition5 = 5; /*sev4*/
+   var indiceOpition6 = 6; /*sev6*/
+   
+
+
+ 
+
+   
+
+
+    
     var faltura = document.getElementsByName('altura')
     var fprop = document.getElementsByName('propagacao')
     var ftempo = document.getElementsByName('operacao')
     var ftam = document.getElementsByName('tamanho')
     var fluz = document.getElementsByName('luz')
-    var severity = document.getElementById('severity')
-    if(fregiao[1].checked && faltura[0].checked ){
-        severity.innerHTML = `Severidade 0` 
-     } else if(faltura[0].checked && fprop[1].checked){
-        severity.innerHTML = `Severidade 0`   
+ 
+
+
+    if(faltura[0].checked && fprop[1].checked){
+      selectElement.selectedIndex = indiceOpition1
      }else if(ftempo[0].checked && fprop[1].checked){
-        severity.innerHTML = `Severidade 1`
+      selectElement.selectedIndex = indiceOpition2
      } else if(ftempo[0].checked && ftam[0].checked && fprop[0].checked ){
-        severity.innerHTML = `Severidade 2`
+      selectElement.selectedIndex = indiceOpition3
      }else if(ftempo[1].checked && ftam[0].checked && fprop[0].checked){
-        severity.innerHTML = `Severidade 3`
+      selectElement.selectedIndex = indiceOpition4
      } else if(fprop[0].checked && ftam[0].checked && fluz[1].checked){
-        severity.innerHTML = `Severidade 4`
+      selectElement.selectedIndex = indiceOpition5
      } else if(fprop[0].checked && ftam[1].checked || fluz[0].checked){
-        severity.innerHTML = `Severidade 5`
+      selectElement.selectedIndex = indiceOpition6 
+    
      }}
 
  
