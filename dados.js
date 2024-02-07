25283b7c201acb4a0c6b07196db6a19e0efad92a
Matheus Oliveira <matheus.oliveira6842@gmail.com>
2024-02-07 20:01:53 -0300

    .

--- dados.js
+++ dados.js
@@ -992,11 +992,119 @@ function carregar2(){
 }
 
 /*Condicionais para Gap ou Ausência de Core - Cascas e Almas*/
+function carregar3(){
+var fgap = document.getElementsByName('gap')
+var fresina = document.getElementsByName('resina')
+var fcolisao = document.getElementsByName('colisao')
+var ftempo = document.getElementsByName('operacao5')
+var fprop = document.getElementsByName('propagacao5')
+var fluz = document.getElementsByName('luz2')
+var fdelam = document.getElementsByName('delaPass')
+
+if(fgap[0].checked && fresina[0].checked && fcolisao[1].checked){
+  selectElement.selectedIndex = indiceOpition1
+} else if(ftempo[0].checked && fprop[1].checked){
+  selectElement.selectedIndex = indiceOpition2
+} else if(fprop[0].checked && fdelam[1].checked){
+  selectElement.selectedIndex = indiceOpition5
+} else if(fluz[0].checked && fprop[0].checked){
+  selectElement.selectedIndex = indiceOpition6 
+}}
 /*Condicionais paraDegrau - Cascas*/
+function carregar4(){
+var ftempo = document.getElementsByName('operacao6')
+var fprop = document.getElementsByName('propagacao6')
+var fluz = document.getElementsByName('luz3')
+var fHD = document.getElementsByName('alturaD')
+var fcompri = document.getElementsByName('compD')
+
+if(fHD[0].checked && fprop[1].checked){
+  selectElement.selectedIndex = indiceOpition1
+} else if(ftempo[0].checked && fcompri[0].checked && fprop[1].checked){
+  selectElement.selectedIndex = indiceOpition2
+} else if(ftempo[0].checked && fcompri[0].checked && fprop[0].checked){
+  selectElement.selectedIndex = indiceOpition3
+} else if(ftempo[1].checked && fcompri[0].checked && fprop[0].checked){
+  selectElement.selectedIndex = indiceOpition4
+}else if(fprop[0].checked && fcompri[0].checked && fluz[1].checked){
+  selectElement.selectedIndex = indiceOpition5
+}
+else if(fprop[0].checked && fcompri[1].checked || fluz[0].checked){
+  selectElement.selectedIndex = indiceOpition6
+}
+}
+
 /*Condicionais para Tecido Seco na superfície - Cascas*/
+function carregar5(){
+var fcompri = document.getElementsByName("compT")
+if(fcompri[0].checked){
+  selectElement.selectedIndex = indiceOpition4
+} else if(fcompri[1].checked){
+  selectElement.selectedIndex = indiceOpition5
+} else if (fcompri[2].checked){
+  selectElement.selectedIndex = indiceOpition6
+}}
 /*Condicionais para Tecido Semi-Seco - Cascas e Almas*/
+
+function carregar6(){
+
+  var ftempo = document.getElementsByName('operacao7')
+  var fprop = document.getElementsByName('propagacao7')
+  var fcompri = document.getElementsByName('compTS')
+
+  if(fcompri[0].checked){
+    selectElement.selectedIndex = indiceOpition1
+  } else if(ftempo[0].checked && fcompri[1].checked && fprop[1].checked){
+    selectElement.selectedIndex = indiceOpition2
+  } else if(ftempo[1].checked && fcompri[1]){
+    selectElement.selectedIndex = indiceOpition4
+  }
+}
 /*Condicionais para Bolhas - Casca*/
-/*Condicionais para Corpo Estranho - Cascas e área de colagem (Somente aplicável para laminados infudidos ou áreas de colagem)			*/
+function carregar7(){
+  var ftamB = document.getElementsByName('tamB')
+  var ftempo = document.getElementsByName('operacao8')
+  var fprop = document.getElementsByName('propagacao8')
+  var fprop1 = document.getElementsByName('propagacao9')
+  var fluzP = document.getElementsByName('luz4')
+  var fluz = document.getElementsByName('luz5')
+  var fcompri = document.getElementsByName('compB')
+
+  if(ftamB[0].checked || ftamB[1].checked){
+    selectElement.selectedIndex = indiceOpition1 
+  } else if(ftempo[0] && fcompri[0] && fprop[1].checked){
+    selectElement.selectedIndex = indiceOpition3
+  } else if(ftempo[1].checked && fcompri[0].checked){
+    selectElement.selectedIndex = indiceOpition4
+  }else if(fcompri[0].checked && fprop[0].checked){
+    selectElement.selectedIndex = indiceOpition4
+  } else if(fprop1[0].checked && fcompri[0].checked && fluzP[0]){
+    selectElement.selectedIndex = indiceOpition5
+  } else if(fprop1[0].checked && fcompri[1].checked || fluz[0].checked){
+    selectElement.selectedIndex = indiceOpition6
+  }}
+
+/*Condicionais para Corpo Estranho - Cascas e área de colagem (Somente aplicável para laminados infudidos ou áreas de colagem)*/
+function carregar8(){
+  var ftam = document.getElementsByName('tamC')
+  var fprop = document.getElementsByName('propagacao10')
+  var ftempo = document.getElementsByName('operacao9')
+  var fmat = document.getElementsByName('mat')
+
+  if(fmat[0].checked && ftempo[0].checked && fprop[1].checked){
+    selectElement.selectedIndex = indiceOpition2
+  } else if(fmat[0].checked && ftempo[0].checked && fprop[0].checked && ftam[0].checked){
+    selectElement.selectedIndex = indiceOpition3
+  } else if (fmat[0].checked && fprop[1].checked && ftam[1].checked ){
+    selectElement.selectedIndex = indiceOpition4
+  }else if(fmat[0].checked){
+    selectElement.selectedIndex = indiceOpition5
+
+  }
+
+
+
+}
 /*Condicionais para Trincas transversais - Cascas e Almas*/
 /*Condicionais para Trincas Longitudinais - Cascas e Almas*/
 /*Condicionais para Trincas Transversais - linhas de colagem*/
